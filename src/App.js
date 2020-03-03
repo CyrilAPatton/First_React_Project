a872caa3132e59cb819ebe5542ec09b1eaedb225
Cyril Patton <cyril_patton@live.com>
2020-03-02 19:34:36 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import HelloWorld from './Components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
